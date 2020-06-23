eb0b27127de6e180084f9115dd68b5d603a9ef60
Reynante Villalongja <reynantevillalongja@gmail.com>
2020-06-24 07:11:59 +0800

    modified page title integrated pages and set up routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,43 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/layout-static', function() {
+    return view('layout-static');
+});
+
+Route::get('/layout-sidenav-light', function() {
+    return view('layout-sidenav-light');
+});
+
+Route::get('/login', function() {
+    return view('login');
+});
+
+Route::get('/password', function() {
+    return view('password');
+});
+
+Route::get('/charts', function() {
+    return view('charts');
+});
+
+Route::get('/register', function() {
+    return view('register');
+});
+
+Route::get('/tables', function() {
+    return view('tables');
+});
+
+Route::get('/401', function() {
+    return view('401');
+});
+
+Route::get('/404', function() {
+    return view('404');
+});
+
+Route::get('/500', function() {
+    return view('500');
+});
